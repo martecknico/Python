f139e6eefa1ef51ceb10f148c7a8050a5377a607
martecknico <118736520+martecknico@users.noreply.github.com>
2023-08-16 15:04:24 -0300

    Delete nome_token.py

--- nome_token.py
+++
@@ -1,9 +0,0 @@
-from random import randrange
-
-nome = input("Qual seu nome? ")
-
-token = randrange(1000,9998,step=2)
-
-comprimento = f"Olá {nome}, o seu token de acesso é {token}"
-
-print(comprimento)
